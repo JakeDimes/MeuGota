a80723a50d8b2a4cc22e192eb48c88268d311bbd
JakeDimes <jakebq2001@outlook.com>
2021-08-26 19:13:17 -0400

    Added debug messages to server.py

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 import socket
 
+
 def handle(client: socket.socket) -> str:
     buffer = int.from_bytes(client.recv(64), 'little')
     return client.recv(buffer).decode('utf-8')
 
+
 def start(port, host: str):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((host, port))
+    print(f"[SERVER] LISTENING ON {host}:{port}")
 
     server.listen(5)
 
