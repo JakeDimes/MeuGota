86b0134818b4e08ef5f426d0f36aa3c810ce8535
JakeDimes <jakebq2001@outlook.com>
2021-08-25 23:09:11 -0400

    Added config and fixed main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import client
 import server
+import json
+
+FILE_PATH = 'config.json'
+
+
+def parse_config():
+    config = json.load(open(FILE_PATH))
+    return config["IP"], config["PORT"]
+
 
 def main():
+
     print("Send or receive? [S/R]?")
     usr = input()
+    ip, port = parse_config()
     if usr == "S":
-        client.start()
+        client.start(port, ip)
     elif usr == "R":
-        server.start()
+        server.start(port, ip)
     else:
         print("[ERROR] INCORRECT SEQUENCE")
 
