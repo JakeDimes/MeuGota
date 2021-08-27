0764f49cb318e3516babfd82897e4dececbf37e9
JakeDimes <jakebq2001@outlook.com>
2021-08-27 12:51:27 -0400

    Fixed compatability issues with mac

--- server.py
+++ server.py
@@ -7,14 +7,19 @@ def handle(client: socket.socket) -> str:
 
 
 def start(port, host: str):
+
+    # create socket and bind to a port
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((host, port))
     print(f"[SERVER] LISTENING ON {host}:{port}")
 
+    # listen and reject up to five connections
     server.listen(5)
 
+    # accept the connection
     client, addr = server.accept()
     print(f"[SERVER] CONNECTION ESTABLISHED WITH {addr}")
     print(f"[MSG] {handle(client)}")
 
+    client.close()
     server.close()
